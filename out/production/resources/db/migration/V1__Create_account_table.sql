e03a846e86cd8c5439691044f2b692a6b36b2750
Philip Vorobyov <phil.vorobyov@gmail.com>
2019-07-20 12:51:39 +0300

    update git ignore

--- out/production/resources/db/migration/V1__Create_account_table.sql
+++
@@ -1,5 +0,0 @@
-create table ACCOUNT (
-    account_number int not null,
-    user_id int not null,
-    amount DECIMAL(13,0) not null
-);
--- out/production/resources/db/migration/V2__Add_accounts.sql
+++
@@ -1,3 +0,0 @@
-insert into ACCOUNT (account_number, user_id, amount) values (111, 1, 1000.0);
-insert into ACCOUNT (account_number, user_id, amount) values (222, 2, 2000.0);
-insert into ACCOUNT (account_number, user_id, amount) values (333, 3, 3000.0);
